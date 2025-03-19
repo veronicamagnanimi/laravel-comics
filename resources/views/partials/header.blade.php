be91586eb895f77129e5ea2f6412e4a70b7f2b6f
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-03-19 17:44:23 +0100

    feat: header with include completed

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comics</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+<style>
+  .navbar-nav .nav-link.active {
+    color: black;
+    text-decoration: underline;
+  }
+
+  .navbar-nav .nav-link {
+    color: gray;
+    text-decoration: none;
+  }
+
+  .navbar-nav .nav-link:hover {
+    color: black;
+    text-decoration: underline;
+  }
+</style>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid d-flex justify-content-between align-items-center">
+    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="" style="width: 50px;">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" href="{{ route('home') }}">Comics</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ request()->routeIs('movies') ? 'active' : '' }}" href="{{ route('movies') }}">Movies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ request()->routeIs('games') ? 'active' : '' }}" href="{{ route('games') }}">Games</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ request()->routeIs('shop') ? 'active' : '' }}" href="{{ route('shop') }}">Shop</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/games', function () {
+    return view('games');
+})->name('games');
+
+Route::get('/movies', function () {
+    return view('movies');
+})->name('movies');
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
